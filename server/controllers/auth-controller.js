f4b371e7b4a0fc0070d011e0d16f33ad3dd3f112
Fazo01 <fg7829098@gmail.com>
2025-11-04 21:02:20 +0500

    complete Bcrypt.js

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -21,10 +21,8 @@ const register=async(req,res)=>{
         return res.status(400).json({msg:"email already exist"})
     }
     //hash the password
-    const saltRound=10
-    const hash_password=await bcrypt.hash(password,saltRound)
 
-    const userCreated=await User.create({username,email,phone,password:hash_password})
+    const userCreated=await User.create({username,email,phone,password})
 
     res.status(200).json({msg:userCreated})
     }catch{
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcrypt')
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
@@ -21,5 +22,22 @@ const userSchema=new mongoose.Schema({
         default:false,
     }
 })
+
+//secure the password with bycrpt
+userSchema.pre('save',async function(next){
+    console.log("pre method:" ,this)
+    const user=this
+    if(!user.isModified("password")){
+        next()
+    }
+    try{
+        const saltRound=await bcrypt.genSalt(10)
+        const hash_password=await bcrypt.hash(user.password,saltRound)
+        user.password=hash_password
+    }catch(error){
+        next(error)
+    }
+})
+
 const User=new mongoose.model("User",userSchema)
 module.exports=User
