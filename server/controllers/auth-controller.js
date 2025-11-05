4ec19299f7d459856cece9f898d5bbe72b4bc18b
Fazo01 <fg7829098@gmail.com>
2025-11-05 20:44:37 +0500

    write login logic in tutorial 13

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,17 +1,15 @@
 const User=require('../models/user-model')
 const bcrypt=require("bcrypt")
+//home
 const home=async(req,res)=>{
     try{
-
-
     res.status(200).send('Welcome to router')
-
     }
     catch(error){
         console.log(object)
     }
 }
-
+//register
 const register=async(req,res)=>{
     try{
     const {username,email,phone,password}=req.body
@@ -34,4 +32,29 @@ const register=async(req,res)=>{
     res.status(400).json('page not found')
     }
 }
-module.exports={home,register}
+
+//user login logic
+const login =async (req,res) => {
+    try{
+        const {email,password}=req.body
+        const UserExist= await User.findOne({email})
+        console.log(UserExist);
+        
+        if(!UserExist){
+            return res.status(400).json({message:"Invalid"})
+        }
+        const user=await bcrypt.compare(password,UserExist.password)
+        if(user){
+            res.status(200).json({
+                msg:"Login Successful",
+                token:await userCreated.generateToken(),
+                userId:userCreated._id.toString(),
+            })
+        }else{
+            res.status(401).json({msg:"Invalid email or password"})
+        }
+    }catch{
+        res.status(500).json("internal server error")
+    }
+}
+module.exports={home,register,login}
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -4,4 +4,5 @@ const authcontrollers=require('../controllers/auth-controller')
 router.route("/").get(authcontrollers.home)//get data
 
 router.route('/register').post(authcontrollers.register)//insert data
+router.route("/login").post(authcontrollers.login)
 module.exports=router
