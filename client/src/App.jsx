811fd6e1b0c5f88a19595469d55a1f16c6672856
Fazo01 <fg7829098@gmail.com>
2025-11-09 16:48:10 +0500

    React Router Dom tutorial 20

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Service from './pages/Service'
 
 const App = () => {
   return (
-    <h1>Hello mern series by thaoa</h1>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/service' element={<Service/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
