18a388e70209cdc37a625c7fa141ed841984c954
Fazo01 <fg7829098@gmail.com>
2025-11-12 17:03:17 +0500

    complete building registeration form in tutorial 22

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { useState } from 'react';
 const Register = () => {
+
+  const[user,setUser]=useState({
+    username:"",
+    email:"",
+    phone:"",
+    password:""
+  })
+
   return (
     <>
       <section>
@@ -16,12 +24,22 @@ const Register = () => {
                 <form>
                   <div>
                     <label htmlFor="username">username</label>
-                    <input type="text" name='username' value={} placeholder='username' id='username' required autoComplete='off'/>
+                    <input type="text" name='username' placeholder='username' id='username' required autoComplete='off' value={user.username}/>
                   </div>
                   <div>
-                    <label htmlFor="username">username</label>
-                    <input type="text" name='username' value={} placeholder='username' id='username' required autoComplete='off'/>
+                    <label htmlFor="email">email</label>
+                    <input type="email" name='email' placeholder='email' id='email' required autoComplete='off' value={user.email}/>
                   </div>
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input type="number" name='phone' placeholder='phone' id='phone' required autoComplete='off' value={user.phone}/>
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input type="password" name='password' placeholder='password' id='password' required autoComplete='off' value={user.password}/>
+                  </div>
+                  <br />
+                  <button type='submit' className='btn btn-submit'>Register Now</button>
                 </form>
               </div>
             </div>
