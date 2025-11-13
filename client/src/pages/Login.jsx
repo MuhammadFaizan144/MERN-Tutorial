750ff3f569b64b4a6c30e52b515f99db927b3049
Fazo01 <fg7829098@gmail.com>
2025-11-13 16:54:18 +0500

    complete login functions in tutorial 22

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  const[user,setUser]=useState({
+      email:"",
+      password:""
+    })
+    //handling the input value
+    const handleInput=(e)=>{
+      // let name=e.
+      console.log(e)
+      let name=e.target.name;
+      let value=e.target.value;
+      setUser({
+        ...user,
+        [name]:value,
+      })
+    }
+  
+    const handleSubmit=(e)=>{
+      e.preventDefault()
+      console.log(user)
+    }
+  
+    return (
+      <>
+        <section>
+          <main>
+            <div className="section-registration">
+              <div className="container grid grid-cols-2">
+                <div className="registeration-image">
+                  <img src="/images/register.png" alt="" width="500" height="500" />
+                </div>
+                <div className="registration-form">
+                  <h1 className='main-heading mb-3'>registeration form</h1>
+                  <br />
+                  <form onSubmit={handleSubmit}>
+                    
+                    <div>
+                      <label htmlFor="email">email</label>
+                      <input type="email" name='email' placeholder='email' id='email' required autoComplete='off' value={user.email} onChange={handleInput}/>
+                    </div>
+                    <div>
+                      <label htmlFor="password">password</label>
+                      <input type="password" name='password' placeholder='password' id='password' required autoComplete='off' value={user.password} onChange={handleInput}/>
+                    </div>
+                    <br />
+                    <button type='submit' className='btn btn-submit'>Register Now</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </main>
+        </section>
+      </>
+    );
+  
 }
 
 export default Login
