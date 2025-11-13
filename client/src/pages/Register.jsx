3709a0c84b1ce2916c1ef693eb9bfcde54429503
Fazo01 <fg7829098@gmail.com>
2025-11-13 16:43:10 +0500

    retrieving user data and storage it in tutorial 22

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,11 +3,22 @@ import { useState } from 'react';
 const Register = () => {
 
   const[user,setUser]=useState({
-    username:"",
+    username:"thapa",
     email:"",
     phone:"",
     password:""
   })
+  //handling the input value
+  const handleInput=(e)=>{
+    // let name=e.
+    console.log(e)
+    let name=e.target.name;
+    let value=e.target.value;
+    setUser({
+      ...user,
+      [name]:value,
+    })
+  }
 
   return (
     <>
@@ -24,19 +35,19 @@ const Register = () => {
                 <form>
                   <div>
                     <label htmlFor="username">username</label>
-                    <input type="text" name='username' placeholder='username' id='username' required autoComplete='off' value={user.username}/>
+                    <input type="text" name='username' placeholder='username' id='username' required autoComplete='off' value={user.username} onChange={handleInput}/>
                   </div>
                   <div>
                     <label htmlFor="email">email</label>
-                    <input type="email" name='email' placeholder='email' id='email' required autoComplete='off' value={user.email}/>
+                    <input type="email" name='email' placeholder='email' id='email' required autoComplete='off' value={user.email} onChange={handleInput}/>
                   </div>
                   <div>
                     <label htmlFor="phone">phone</label>
-                    <input type="number" name='phone' placeholder='phone' id='phone' required autoComplete='off' value={user.phone}/>
+                    <input type="number" name='phone' placeholder='phone' id='phone' required autoComplete='off' value={user.phone} onChange={handleInput}/>
                   </div>
                   <div>
                     <label htmlFor="password">password</label>
-                    <input type="password" name='password' placeholder='password' id='password' required autoComplete='off' value={user.password}/>
+                    <input type="password" name='password' placeholder='password' id='password' required autoComplete='off' value={user.password} onChange={handleInput}/>
                   </div>
                   <br />
                   <button type='submit' className='btn btn-submit'>Register Now</button>
