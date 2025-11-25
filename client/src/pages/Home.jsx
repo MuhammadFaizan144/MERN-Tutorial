c72d81efb4bac750fa5a709a59bffc5e32dd47ab
Fazo01 <fg7829098@gmail.com>
2025-11-25 16:54:08 +0500

    Crafting the Home Section on HomePage in tutorial 24

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-
 const Home = () => {
   return (
     <>
-    <h1>Home</h1>
+    <main className='section-hero'>
+      <div className="container grid grid-two-cols">
+        <div className="hero-content">
+          <p>We are the World Best IT Company</p>
+          <h1>Welcome to Thapa Technical</h1>
+              <p>
+                Are you ready to take your business to the next level with
+                cutting-edge IT solutions? Look no further! At Thapa Technical,
+                we specialize in providing innovative IT services and solutions
+                tailored to meet your unique needs.
+              </p>
+          <div className="btn btn-group">
+            <a href="/contact">
+              <button className='btn'>connect now</button>
+            </a>
+            <a href="/services">
+              <button className='btn'>learn more</button>
+            </a>
+          </div>
+        </div>
+        {/* hero images */}
+        <div className="hero-image">
+          <img src="/images/home.png" alt="home" width='400' height='500'/>
+        </div>
+      </div>
+    </main>
     </>
   )
 }
-
 export default Home
