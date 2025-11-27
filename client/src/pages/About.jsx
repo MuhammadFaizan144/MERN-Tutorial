b16275fbc87cc5f3c3a471745057793fed98fa12
Fazo01 <fg7829098@gmail.com>
2025-11-27 15:20:05 +0500

    initialize service page in tutorial 24

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,64 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <>
+    <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>We are the World Best IT Company</p>
+              <h1>Welcome to Thapa Technical</h1>
+              <p>
+                Are you ready to take your business to the next level with
+                cutting-edge IT solutions? Look no further! At Thapa Technical,
+                we specialize in providing innovative IT services and solutions
+                tailored to meet your unique needs.
+              </p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">connect now</button>
+                </a>
+                <a href="/service">
+                  <button className="btn secondary-btn">learn more</button>
+                </a>
+              </div>
+            </div>
+
+            {/* hero images  */}
+            <div className="hero-image">
+              <img
+                src="/images/home.png"
+                alt="coding together"
+                width="400"
+                height="500"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+      {/* second section */}
+      <section className='section-analytics'>
+        <div className="container grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>registered companies</p>
+          </div>
+          <div className="div1">
+            <h2>100.00+</h2>
+            <p>Happy Clients</p>
+          </div>
+          <div className="div1">
+            <h2>500+</h2>
+            <p>Well known Developers</p>
+          </div>
+          <div className="div1">
+            <h2>24/7</h2>
+            <p>service</p>
+          </div>
+        </div>
+      </section>
+  </>
+)
 }
 
 export default About
