448edb55da5bdbf64240c01b66fe66937a3c05ef
Fazo01 <fg7829098@gmail.com>
2025-11-28 18:28:10 +0500

    add analytics components in tutorial 25

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Analytics from '../Components/Analytics'
 const Home = () => {
   return (
     <>
@@ -37,26 +38,7 @@ const Home = () => {
         </section>
       </main>
       {/* second section */}
-      <section className='section-analytics'>
-        <div className="container grid grid-four-cols">
-          <div className="div1">
-            <h2>50+</h2>
-            <p>registered companies</p>
-          </div>
-          <div className="div1">
-            <h2>100.00+</h2>
-            <p>Happy Clients</p>
-          </div>
-          <div className="div1">
-            <h2>500+</h2>
-            <p>Well known Developers</p>
-          </div>
-          <div className="div1">
-            <h2>24/7</h2>
-            <p>service</p>
-          </div>
-        </div>
-      </section>
+      <Analytics/>
       {/* 3rd section  */}
       <section className="section-hero">
         <div className="container grid grid-two-cols">
