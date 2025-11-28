6edaa395da80b796c7f21c02e01679d42c3f05fb
Fazo01 <fg7829098@gmail.com>
2025-11-28 17:06:11 +0500

    functions of contact page in tutorial 24

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
 const Contact = () => {
+  const[contact,setContact]=useState({
+    username:"",
+    email:"",
+    message:"",
+  });
+  const handleInput=(e)=>{
+    const name=e.target.name;
+    const value=e.target.value;
+    setContact({
+      ...contact,
+      [name]:value,
+    })
+    // 2nd method
+    // setContact((prev)=>({
+    //   ...prev,
+    //   [name]:value,
+    // }));
+  };
   return (
     <>
       <section className='section-contect'>
@@ -19,15 +37,15 @@ const Contact = () => {
                   <label htmlFor="username">
                     username
                   </label>
-                  <input type="text" name="username" id="username" autoComplete='off' required/>
+                  <input type="text" name="username" id="username" autoComplete='off' required value={contact.username} onChange={handleInput}/>
                 </div>
                 <div>
                   <label htmlFor="email">email</label>
-                  <input type="email" name="email" id="email" autoComplete='off' required/>
+                  <input type="email" name="email" id="email" autoComplete='off' required value={contact.email} onChange={handleInput}/>
               </div>
               <div>
                 <label htmlFor="message">message</label>
-                <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                <textarea name="message" id="message" cols="30" rows="10" autoComplete='off' required value={contact.message} onChange={handleInput}></textarea>
               </div>
               <div>
                 <button type="submit">submit</button>
