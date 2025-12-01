b5fb00ce7d4b45db075d7ac63d8946473e00e066
Fazo01 <fg7829098@gmail.com>
2025-12-01 15:35:57 +0500

    installing cors in tutorial 26

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const express=require("express")
+const cors=require('cors')
 const app=express()
 const authRoute=require('./router/auth-router')
 const contactRoute=require('./router/contact-router')
 const connectDb=require('./utils/db')
 const errorMiddleware = require('./middleware/error-middleware')
+
+// lets tackle cors
+app.use(cors)
 
 app.use(express.json())//middleware
 
