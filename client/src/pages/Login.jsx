92d0e6fe6884574befa93720c66dc238dfc12356
Fazo01 <fg7829098@gmail.com>
2025-12-02 15:30:00 +0500

    complete adding login functionality in tutorial 27

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,22 +1,45 @@
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+const URL='http://localhost:3000/api/auth/login'
 const Login = () => {
-    const[user,setuser]=useState({
-      email:"h",
+    const[user,setUser]=useState({
+      email:"",
       password:""
     })
+    const navigate=useNavigate()
     const handleInput=(e)=>{
       console.log(e)
       let name=e.target.name
       let value=e.target.value
-      setuser({
+      setUser({
         ...user,
         [name]:value
       })
     }
-    const handleSubmit=(e)=>{
+    const handleSubmit=async (e)=>{
       e.preventDefault()
-      console.log(e)
+      console.log(user)
+      try{
+        const response=await fetch(URL,{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(user)
+        
+      })
+      console.log("login form", response)
+      if (response.ok) {
+        alert("Login successful")
+          setUser({email:"",password:""})
+          
+        }else{
+          console.log("Error in response")
+        }
+      }catch(error){
+        console.log("Login connection error with backend",error)
+      }
     }
     return (
       <>
