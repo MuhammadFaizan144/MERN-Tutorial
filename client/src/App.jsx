2d358ce86ea43c4fdabf808629ce97170c1c356b
Fazo01 <fg7829098@gmail.com>
2025-12-04 21:33:07 +0500

    Creating logout route in tutorial 29

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Service from './pages/Service'
 import Navbar from './Components/Navbar'
 import Error from './pages/error'
 import Footer from './Components/Footer/Footer'
+import Logout from './pages/Logout'
 const App = () => {
   return (
     <>
@@ -21,6 +22,7 @@ const App = () => {
           <Route path='/service' element={<Service/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
+          <Route path='/logout' element={<Logout/>}/>
           <Route path='*' element={<Error/>}/>
         </Routes>
       <Footer/>
