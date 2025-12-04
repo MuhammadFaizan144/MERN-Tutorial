6ae0d265b2adc2b8b306f1fdaf2ce385ad742cc2
Fazo01 <fg7829098@gmail.com>
2025-12-04 21:42:01 +0500

    defining logout functionality in context in tutorial 29

--- client/src/pages/Logout.jsx
+++ client/src/pages/Logout.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
+import { useAuth } from '../store/auth'
+
 const Logout = () => {
+    const {LogoutUser}=useAuth()
     useEffect(() => {
       LogoutUser()
     }, [LogoutUser])
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,9 +1,19 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 export const AuthContext=createContext()
 export const AuthProvider=({children})=>{
+
+    const [token,settoken]=useState(localStorage.getItem("token"))
+
     const storetokenInLS=(serverToken)=>{
         return localStorage.setItem('token',serverToken)
     }
+
+    //logout functionality
+    const LogoutUser=()=>{
+        settoken("")
+        return localStorage.removeItem("token")
+    }
+
 
     return (<AuthContext.Provider value={storetokenInLS}>
         {children}
