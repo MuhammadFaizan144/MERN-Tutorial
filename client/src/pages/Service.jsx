20158dddb58ed8df55daedfc2db44a94ccd8d429
Fazo01 <fg7829098@gmail.com>
2025-12-10 16:57:11 +0500

    getting services data in the services page utilizing map to create content in tutorial 34

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
+import { useAuth } from '../store/auth'
 
 const Service = () => {
+  const {services}=useAuth()
   return (
     <section className='section-services'>
       <div className="container">
         <h1 className='main-heading'>Services</h1>
       </div>
       <div className="container grid grid-three-cols">
-        <div className="card">
+        {
+          services.map((curElem,index)=>{
+            const {price,description,provider,service}=curElem;
+            return(
+        <div className="card" key={index}>
           <div className="card-img">
             <img src="/images/design.png" alt="our services info" width="200"/>
           </div>
           <div className="card-details">
             <div className="gird grid-two-cols">
-              <p>provider</p>
-              <p>price</p>
+              <p>{provider}</p>
+              <p>{price}</p>
             </div>
-            <h2>service</h2>
-            <p>description</p>
+            <h2>{service}</h2>
+            <p>{description}</p>
           </div>
-        </div>
+        </div>)
+          })
+        }
       </div>
     </section>
   )
