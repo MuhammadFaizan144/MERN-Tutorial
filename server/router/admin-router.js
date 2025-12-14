9d8058a3650389dba2295da864801c980ef81eb0
Fazo01 <fg7829098@gmail.com>
2025-12-14 16:45:19 +0500

    continued coding in our controller file in tutorial 39

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,3 +1,5 @@
+const getAllUsers = require("../controllers/admin-controller")
+
 const express=require(express)
 const router=express.Router()
-router.route('/user').get()
+router.route('/user').get(getAllUsers)
