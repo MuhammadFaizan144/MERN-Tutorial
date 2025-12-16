f8d3cf5234074e5c0cb65eee2f95bb81a12d8b5e
Fazo01 <fg7829098@gmail.com>
2025-12-16 15:58:00 +0500

    utilizing req.user.understanding the req.user property for admin checks in tutorial 42

--- server/middleware/admin-middleware.js
+++ server/middleware/admin-middleware.js
@@ -1,7 +1,9 @@
 const adminMiddleware=async (req,res,next) => {
     try {
-        
+        console.log(req.user)
+        res.status(200).json({msg:req.user.isAdmin})
     } catch (error) {
         next(error)
     }
 }
+module.exports=adminMiddleware
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,7 +1,8 @@
 const {getAllUsers,getAllContacts} = require("../controllers/admin-controller")
+const adminMiddleware = require("../middleware/admin-middleware")
 const authMiddlerware=require('../middleware/auth-middlerware')
 const express=require("express")
 const router=express.Router()
-router.route('/users').get(authMiddlerware,getAllUsers)
+router.route('/users').get(authMiddlerware,adminMiddleware,getAllUsers)
 router.route('/contacts').get(authMiddlerware,getAllContacts)
 module.exports=router
