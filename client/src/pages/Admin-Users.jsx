5e9a1d7717116a0fb83f8d7c9ec5387f90d7cc36
Fazo01 <fg7829098@gmail.com>
2025-12-17 17:09:11 +0500

    Invoke the user route to retrieve all data in tutorial 43

--- client/src/pages/Admin-Users.jsx
+++ client/src/pages/Admin-Users.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 const AdminUsers = () => {
+    const getAllUsersData=async () => {
+        try {
+            const response=await fetch('http://localhost:3000/api/admin/users',{
+                
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(()=>{
+        getAllUsersData()
+    },[])
   return (
     <div>AdminUsers</div>
   )
