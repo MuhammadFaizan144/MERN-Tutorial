e56cd3802c2c3a12bbd55989316449424fd8e541
Fazo01 <fg7829098@gmail.com>
2025-12-17 16:56:38 +0500

    craftung the admin navbar and outlet in tutorial 43

--- client/src/Components/layouts/Admin-Layouts.jsx
+++ client/src/Components/layouts/Admin-Layouts.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-
+import { NavLink, Outlet } from 'react-router-dom'
 const AdminLayouts = () => {
-  return (
+  return (<>
     <header>
         <div className="container">
             <nav>
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li><NavLink to="/admin/users">users</NavLink></li>
+                    <li><NavLink to="/admin/contacts">contacts</NavLink></li>
+                    <li><NavLink to="/service">services</NavLink></li>
+                    <li><NavLink to="/">Home</NavLink></li>
                 </ul>
             </nav>
         </div>
     </header>
+    <Outlet/>
+    </>
   )
 }
 
