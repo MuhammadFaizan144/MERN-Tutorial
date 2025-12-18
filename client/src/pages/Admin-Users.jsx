6a8ce33c30a6482c0fffc9e49fdaf9fa41e2c6ec
Fazo01 <fg7829098@gmail.com>
2025-12-18 21:58:13 +0500

    Employee the map to iterate through users on the user page in tutorial 44

--- client/src/pages/Admin-Users.jsx
+++ client/src/pages/Admin-Users.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useAuth } from '../store/auth'
 const AdminUsers = () => {
-    const {authorizationToken}=useAuth()
-    const getAllUsersData=async () => {
+    const [users, setUsers] = useState([])
+    const { authorizationToken } = useAuth()
+    const getAllUsersData = async () => {
         try {
-            const response=await fetch('http://localhost:3000/api/admin/users',{
-                method:"GET",
-                headers:{
-                    Authorization:authorizationToken
+            const response = await fetch("http://localhost:3000/api/admin/users", {
+                method: "GET",
+                headers: {
+                    Authorization: authorizationToken
                 }
-            })
-            const data=await response.json()
+            });
+
+            const data = await response.json()
             console.log(`user ${data}`)
+            setUsers(data);
         } catch (error) {
             console.log(error)
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         getAllUsersData()
-    },[])
-  return (
-    <div>AdminUsers</div>
-  )
+    }, [])
+    return (
+        <>{users.map((curUser, index) => {
+            return <h2 key={index}>{curUser.email}</h2>
+        })}</>
+    )
 }
 
 export default AdminUsers
