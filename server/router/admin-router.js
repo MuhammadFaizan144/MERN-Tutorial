ee86ca4e536d98c9433a969861a1bbfb77a9e62b
Fazo01 <fg7829098@gmail.com>
2025-12-21 16:39:44 +0500

    Creating a Backend Route for Update Functionality in tutorial 48

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,4 +1,4 @@
-const {getAllUsers,getAllContacts,deleteUserById,getUsersById} = require("../controllers/admin-controller")
+const {getAllUsers,getAllContacts,deleteUserById,getUsersById,updateUserById} = require("../controllers/admin-controller")
 const adminMiddleware = require("../middleware/admin-middleware")
 const authMiddlerware=require('../middleware/auth-middlerware')
 
@@ -6,6 +6,7 @@ const express=require("express")
 const router=express.Router()
 router.route('/users').get(authMiddlerware,adminMiddleware,getAllUsers)
 router.route('/users/:id').get(authMiddlerware,adminMiddleware,getUsersById)
+router.route("/users/update/:id").patch(authMiddlerware,adminMiddleware,updateUserById)
 router.route('/users/delete/:id').delete(authMiddlerware,adminMiddleware,deleteUserById)
 router.route('/contacts').get(authMiddlerware,adminMiddleware,getAllContacts)
 module.exports=router
